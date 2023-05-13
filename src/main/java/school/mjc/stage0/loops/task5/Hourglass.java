04bddf4ae15ac45756ebd2f8bbe1da5ec7094bad
Fanis <mrduck597@gmail.com>
2023-05-13 18:49:45 +0500

    test1

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,15 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
-
+        for ( int y=1; y<=height; y++) {
+            for (int x = 1; x <= height; x++) {
+                if ((x >= y && x <= height + 1 - y) || (x<=y && x>= height + 1 - y)) {
+                    System.out.print("8");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Square.java
+++ src/main/java/school/mjc/stage0/loops/task5/Square.java
@@ -2,6 +2,15 @@ package school.mjc.stage0.loops.task5;
 
 public class Square {
     public void printSquareFrom8s(int sideLength){
-
+        for (int y = 1; y <= sideLength; y++) {
+            for (int x = 1; x <= sideLength; x++) {
+                if ((y == 1 || y == sideLength || x == 1 || x == sideLength)) {
+                    System.out.print("8");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("");
+        }
     }
 }
