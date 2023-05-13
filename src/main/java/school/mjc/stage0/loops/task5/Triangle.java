e4918d1176e5c5aa14598d8964778746f944abc9
Fanis <mrduck597@gmail.com>
2023-05-13 23:25:06 +0500

    test2

--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -2,15 +2,16 @@ package school.mjc.stage0.loops.task5;
 
 public class Triangle {
     public void printTriangle(int cathetusLength) {
-        for ( int y=0; y<=cathetusLength; y++){
+
+        for ( int y=1; y<=cathetusLength; y++){
             for (int x=1; x<=cathetusLength; x++){
-                if(x<=y){
-                    System.out.print("8");
-                } else {
-                    System.out.print(" ");
+                System.out.print("8");
+                if (x==y){
+                    break;
                 }
             }
             System.out.println();
         }
+
     }
 }
